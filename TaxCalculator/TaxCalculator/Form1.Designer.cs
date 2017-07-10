fd062b45c74a85bd4870360b6ba12a8e93bf0789
kadir4444 <kadir4444@live.co.uk>
2017-07-10 22:18:15 +0100

    First Commit
    Only added taxes for UK, USA and China.

---
+++ TaxCalculator/TaxCalculator/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace TaxCalculator
+{
+    partial class frm1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtAmount = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cmbCountry = new System.Windows.Forms.ComboBox();
+            this.lblTaxOutput = new System.Windows.Forms.Label();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "AMOUNT:";
+            // 
+            // txtAmount
+            // 
+            this.txtAmount.Location = new System.Drawing.Point(125, 33);
+            this.txtAmount.Name = "txtAmount";
+            this.txtAmount.Size = new System.Drawing.Size(135, 22);
+            this.txtAmount.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(24, 83);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "COUNTRY:";
+            // 
+            // cmbCountry
+            // 
+            this.cmbCountry.FormattingEnabled = true;
+            this.cmbCountry.Items.AddRange(new object[] {
+            "United Kingdom",
+            "USA",
+            "China",
+            "France",
+            "Germany",
+            "Brazil",
+            "Japan",
+            "India"});
+            this.cmbCountry.Location = new System.Drawing.Point(125, 83);
+            this.cmbCountry.Name = "cmbCountry";
+            this.cmbCountry.Size = new System.Drawing.Size(135, 24);
+            this.cmbCountry.TabIndex = 3;
+            // 
+            // lblTaxOutput
+            // 
+            this.lblTaxOutput.AutoSize = true;
+            this.lblTaxOutput.Location = new System.Drawing.Point(122, 131);
+            this.lblTaxOutput.Name = "lblTaxOutput";
+            this.lblTaxOutput.Size = new System.Drawing.Size(144, 17);
+            this.lblTaxOutput.TabIndex = 4;
+            this.lblTaxOutput.Text = "#################";
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(27, 172);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(233, 61);
+            this.btnCalculate.TabIndex = 5;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(267, 172);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(88, 60);
+            this.btnClear.TabIndex = 6;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // frm1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 262);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.lblTaxOutput);
+            this.Controls.Add(this.cmbCountry);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtAmount);
+            this.Controls.Add(this.label1);
+            this.Name = "frm1";
+            this.Text = "MAK_TaxCalc";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtAmount;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cmbCountry;
+        private System.Windows.Forms.Label lblTaxOutput;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Button btnClear;
+    }
+}
+
---
+++ TaxCalculator/TaxCalculator/Form1.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TaxCalculator
+{
+    public partial class frm1 : Form
+    {
+        public frm1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtAmount.Text = "";
+            cmbCountry.Text = "";
+            lblTaxOutput.Text = "#################";
+        }
+
+        int Amount;
+        int tax;
+        String Country;
+        double rate;
+        double CountryTax;
+        String total;
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (txtAmount.Text != "")
+                {
+                    Amount = Convert.ToInt32(txtAmount.Text);
+                    Country = cmbCountry.Text;
+
+                    switch (Country)
+                    {
+                        case "United Kingdom":
+                            if (Amount <= 11500)
+                            {
+                                rate = 0;
+                            }
+                            else if (Amount >= 11501 && Amount <= 45000)
+                            {
+                                rate = 20;
+                            }
+                            else if (Amount >= 45001 && Amount <= 150000)
+                            {
+                                rate = 40;
+                            }
+                            else if (Amount > 150000)
+                            {
+                                rate = 45;
+                            }
+                            CountryTax = Amount - (Amount / rate);
+                            total = Convert.ToString(CountryTax);
+                            lblTaxOutput.Text = "Tax Due: " + total;
+                            break;
+
+                        case "USA":
+                            if (Amount <= 9325)
+                            {
+                                rate = 10;
+                            }
+                            else if (Amount >= 9326 && Amount <= 37950)
+                            {
+                                rate = 15;
+                            }
+                            else if (Amount >= 37951 && Amount <= 91900)
+                            {
+                                rate = 25;
+                            }
+                            else if (Amount >= 91901 && Amount <= 191650)
+                            {
+                                rate = 28;
+                            }
+                            else if (Amount >= 191651 && Amount <= 416700)
+                            {
+                                rate = 33;
+                            }
+                            else if (Amount >= 416701 && Amount <= 418400)
+                            {
+                                rate = 35;
+                            }
+                            else if (Amount > 418400)
+                            {
+                                rate = 39.6;
+                            }
+                            CountryTax = Amount - (Amount / rate);
+                            total = Convert.ToString(CountryTax);
+                            lblTaxOutput.Text = "Tax Due: " + total;
+                            break;
+                        case "China":
+                            //Enter deetss
+                            if (Amount < 1500)
+                            {
+                                rate = 2.5;
+                            }
+                            else if (Amount >= 9326 && Amount <= 37950)
+                            {
+                                rate = 15;
+                            }
+                            else if (Amount >= 37951 && Amount <= 91900)
+                            {
+                                rate = 25;
+                            }
+                            else if (Amount >= 91901 && Amount <= 191650)
+                            {
+                                rate = 28;
+                            }
+                            else if (Amount >= 191651 && Amount <= 416700)
+                            {
+                                rate = 33;
+                            }
+                            else if (Amount >= 416701 && Amount <= 418400)
+                            {
+                                rate = 35;
+                            }
+                            else if (Amount > 418400)
+                            {
+                                rate = 39.6;
+                            }
+                            break;
+                        case "France":
+                            lblTaxOutput.Text = "g";
+                            break;
+                        case "Germany":
+                            lblTaxOutput.Text = "h";
+                            break;
+                        case "Brazil":
+                            lblTaxOutput.Text = "i";
+                            break;
+                        case "Japan":
+                            lblTaxOutput.Text = "j";
+                            break;
+                        case "India":
+                            lblTaxOutput.Text = "k";
+                            break;
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Populate the Amount with currency");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("ERROR Message: " + ex);
+            }
+        }
+    }
+}
